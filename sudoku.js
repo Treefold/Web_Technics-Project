d31bda2572e344867eda4ba17dcdc852297a1df5
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2020-01-02 20:10:24 +0200

    Solvers
    can solve 2^2^2 & 3^3^3 & 4^4^4 Sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,25 +1,25 @@
 (function () {
   //create Sudoku game table
-  var body = document.body;
+  let body = document.body;
   body.style.backgroundColor = "#ccffff";
-  var tableSudoku = document.createElement ('table');
+  let tableSudoku = document.createElement ('table');
   tableSudoku.id = "Sudoku";
   body.appendChild (tableSudoku);
 
   //Instructions
   body.appendChild (document.createElement ('br'));
-  var instructions = document.createElement ('h3');
+  let instructions = document.createElement ('h3');
   instructions.innerHTML = "Chose one number from below and click where \nit should be in the Sudoku you want to solve";
   body.appendChild (instructions);
   
   //create Sudoku choices' list
-  var tableOfChoices = document.createElement ('table');
+  let tableOfChoices = document.createElement ('table');
   tableOfChoices.id = "SudokuChoices";
   body.appendChild (tableOfChoices);
   
   //Restart button = Clear Sudoku
   body.appendChild (document.createElement ('br'));
-  var restartb = document.createElement ('button');
+  let restartb = document.createElement ('button');
   restartb.style.backgroundColor = 'yellow';
   restartb.addEventListener ("click", clear);
   restartb.innerHTML = "Clear Sudoku";
@@ -27,7 +27,7 @@
   body.appendChild (restartb);
   
   //Solve button
-  var solveb = document.createElement('button');
+  let solveb = document.createElement('button');
   solveb.style.backgroundColor = 'yellow';
   solveb.addEventListener("click", solve);
   solveb.innerHTML = "Solve Sudoku";
@@ -35,31 +35,31 @@
   body.appendChild(solveb);
   
   //min sudoku button
-  var minb = document.createElement('button');
+  let minb = document.createElement('button');
   minb.style.backgroundColor = 'yellow';
   minb.addEventListener("click",  function(){deleteAll();createSudoku("Min");});
-  minb.innerHTML = "min Sudoku";
+  minb.innerHTML = "new Min Sudoku";
   minb.appendChild(document.createTextNode('\u0020'));
   body.appendChild(minb);
 
   //decimal sudoku button
-  var decb = document.createElement('button');
+  let decb = document.createElement('button');
   decb.style.backgroundColor = 'yellow';
   decb.addEventListener("click", function(){deleteAll();createSudoku("Dec");});
-  decb.innerHTML = "dec Sudoku";
+  decb.innerHTML = "new Dec Sudoku";
   decb.appendChild(document.createTextNode('\u0020'));
   body.appendChild(decb);
   
   //hex button
-  var deleteAllb = document.createElement('button');
+  let deleteAllb = document.createElement('button');
   deleteAllb.style.backgroundColor = 'yellow';
   deleteAllb.addEventListener("click", function(){deleteAll();createSudoku("Hex");});
-  deleteAllb.innerHTML = "hex Sudoku";
+  deleteAllb.innerHTML = "new Hex Sudoku";
   deleteAllb.appendChild(document.createTextNode('\u0020'));
   body.appendChild(deleteAllb);
   
   // // Delete button
-  // var deleteAllb = document.createElement('button');
+  // let deleteAllb = document.createElement('button');
   // deleteAllb.style.backgroundColor = 'yellow';
   // deleteAllb.addEventListener("click", deleteAll);
   // deleteAllb.innerHTML = "Delete All";
@@ -70,7 +70,7 @@
 }  
 )();
 
-var g_SudokuZoneSize, g_SudokuLineSize;
+var g_SudokuZoneSize, g_SudokuLineSize, g_sudokuType;
 
 function createSudoku (type = "Dec") {
 
@@ -81,15 +81,17 @@ function createSudoku (type = "Dec") {
     default:    console.log ("Create Sudoku ERROR: UNKNOWN TYPE OF SUDOKU!"); return;
   }
 
+  g_sudokuType = type;
+
   g_SudokuLineSize = g_SudokuZoneSize * g_SudokuZoneSize;
 
-  var tableSudoku = document.getElementById ('Sudoku');
-  for (var i = 1; i <= g_SudokuLineSize; ++i) {
-    var tr = document.createElement('tr');
+  let tableSudoku = document.getElementById ('Sudoku');
+  for (let i = 1; i <= g_SudokuLineSize; ++i) {
+    let tr = document.createElement('tr');
     tr.id = "line" + String(i);
-    for (var j = 1; j <= g_SudokuLineSize; ++j) {
-      var td = document.createElement('td');
-      var button = document.createElement('button');
+    for (let j = 1; j <= g_SudokuLineSize; ++j) {
+      let td = document.createElement('td');
+      let button = document.createElement('button');
       button.classList.add("box");
       button.style.backgroundColor = 'yellow';
       button.style.textAlign = "center";
@@ -103,7 +105,7 @@ function createSudoku (type = "Dec") {
       td.appendChild(button);
       tr.appendChild(td);
       if (j % g_SudokuZoneSize == 0 && j != g_SudokuLineSize){
-        var tdb = document.createElement('td');
+        let tdb = document.createElement('td');
         tdb.style.textAlign = "center";
         tdb.innerHTML = "|";
         tdb.appendChild(document.createTextNode('\u0020'));
@@ -113,9 +115,9 @@ function createSudoku (type = "Dec") {
     
     tableSudoku.appendChild(tr);
     if (i % g_SudokuZoneSize == 0 && i != g_SudokuLineSize){
-      var trb = document.createElement('tr');
-      for (var j = 1; j < g_SudokuZoneSize * (g_SudokuZoneSize + 1); ++j) {
-        var tdb = document.createElement('td');
+      let trb = document.createElement('tr');
+      for (let j = 1; j < g_SudokuZoneSize * (g_SudokuZoneSize + 1); ++j) {
+        let tdb = document.createElement('td');
         tdb.style.textAlign = "center";
         tdb.innerHTML = (j % (g_SudokuZoneSize + 1) == 0) ? "+" : "---";
         tdb.appendChild(document.createTextNode('\u0020'));
@@ -125,15 +127,15 @@ function createSudoku (type = "Dec") {
     }
   }
   
-  var listChoice = document.createElement('tr');
-  var last = g_SudokuLineSize + 1;
-  for (i = 1; i <= last; ++i){
-    var choicePlace = document.createElement('td');
-    var choice = document.createElement('button');
+  let listChoice = document.createElement('tr');
+  let last = g_SudokuLineSize + ((type != "Hex") ? 1 : 0);
+  for (i = (type == "Hex") ? 0 : 1; i <= last; ++i){
+    let choicePlace = document.createElement('td');
+    let choice = document.createElement('button');
     choice.style.textAlign = "center";
     choice.classList.add("choice");
     choice.style.backgroundColor = 'yellow';
-    choice.id = choice.innerHTML = (i != last) ? String(i) : "Delete";
+    choice.id = choice.innerHTML = (i != last) ? i.toString(16) : "Delete";
     choice.addEventListener("click", function(){selectChoice(this);});
     choice.appendChild(document.createTextNode('\u0020'));
     choicePlace.appendChild(choice);
@@ -147,19 +149,28 @@ function zoneOf (i, j){
        + Math.trunc((j - 1) / g_SudokuZoneSize) + 1;
 }
 
+
+
+function deleteRec (obj) {
+  while (obj.childNodes.length > 0) {
+    deleteRec (obj.firstChild);
+    obj.removeChild (obj.firstChild);
+  }
+}
+
 function deleteAll() {
-  for (var del of document.querySelectorAll("#Sudoku td, #SudokuChoices td, #Sudoku tr, #SudokuChoices tr")) {
-    del.remove (del.firstChild);
+  for (let table of document.querySelectorAll("#Sudoku, #SudokuChoices")) {
+    deleteRec (table)
   }
 }
 
 function clear(){
-  var choises = document.getElementsByClassName("choice");
-  for (var i = 0; i < g_SudokuLineSize; ++i){
+  let choises = document.getElementsByClassName("choice");
+  for (let i = 0; i < g_SudokuLineSize; ++i){
     choises[i].style.backgroundColor = 'yellow';
   }
-  var boxes = document.getElementsByClassName("box");
-  for (var i = 0; i < g_SudokuLineSize * g_SudokuLineSize; ++i){
+  let boxes = document.getElementsByClassName("box");
+  for (let i = 0; i < g_SudokuLineSize * g_SudokuLineSize; ++i){
     boxes[i].style.backgroundColor = 'yellow';
     boxes[i].innerHTML = '#';
   }
@@ -172,7 +183,7 @@ function selectChoice (choice){
     return;
   }
   // deselect all selecte choises
-  var listChoice = document.getElementsByClassName('choice');
+  let listChoice = document.getElementsByClassName('choice');
   for (i = 0; i <= g_SudokuLineSize; ++i){
     listChoice[i].style.backgroundColor = 'yellow';
   }
@@ -183,17 +194,17 @@ function testBox (box, choice){ //test if the choice fits in the box
   //box.class[]: "box", "zonaZ", "lineL", "colC"
   
   //test zone
-  var zone = document.getElementsByClassName(box.classList[1]);
+  let zone = document.getElementsByClassName(box.classList[1]);
   for( i in zone){
     if (zone[i].innerHTML === choice.innerHTML){ return false;}
   }
   //test line
-  var line = document.getElementsByClassName(box.classList[2]);
+  let line = document.getElementsByClassName(box.classList[2]);
   for( i in line){
     if (line[i].innerHTML === choice.innerHTML){ return false;}
   }
   //test col
-  var col = document.getElementsByClassName(box.classList[3]);
+  let col = document.getElementsByClassName(box.classList[3]);
   for( i in col){
     if (col[i].innerHTML === choice.innerHTML){ return false;}
   }
@@ -201,17 +212,18 @@ function testBox (box, choice){ //test if the choice fits in the box
   return true;
 }
 
-function selectBox (box){
-  var listChoice = document.getElementsByClassName('choice');
-  var noChoice = true; //we start by believing there is no choice selected
+function selectBox (box) {
+  let listChoice = document.getElementsByClassName('choice');
+  let noChoice = true; //we start by believing there is no choice selected
+  let choice;
   // testing my theory
-  for (var i = 0; i <= g_SudokuLineSize && noChoice; ++i){
+  for (let i = 0; i <= g_SudokuLineSize && noChoice; ++i){
     if (listChoice[i].style.backgroundColor === 'green'){
       noChoice = false;
-      var choice = listChoice[i];
+      choice = listChoice[i];
     }
   }
-  if (noChoice){return;} //no choice selected => no action needed
+  if (noChoice){return;} // no choice selected => no action needed
   if (box.innerHTML === choice.innerHTML){return;} // same number => wish granted already => no action needed
   if (choice.id == "Delete"){ box.innerHTML = "#"; return;} //just delete the curent number from the box
   if (testBox (box, choice)){ //test if the choice fits in the box
@@ -220,11 +232,13 @@ function selectBox (box){
 }
 
 function solve(){
-  var mat = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]];
+  let startSolve = new Date();
+  var mat = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]];
   var tra = [0];
   var z = [0]
+  tra["# "] = tra["#"] = 0;
   for (var i = 0; i <= g_SudokuLineSize; ++i){
-    tra[String(i) + " "] = i;
+    tra[i.toString(16) + " "] = i + (g_sudokuType != "Hex" ? 0 : 1);
     mat[i][0] = mat[0][i] = z[i] = 0;
   }
   var boxes = document.getElementsByClassName("box");
@@ -241,17 +255,12 @@ function solve(){
   
   function sig() // select all sure
   {
-    var a = 0;
-    var b = 0;
-    var k = 0;
-    var l = 0;
-    var i = 0;
-    var j = 0;
+    let a = b = k = l = i = j = 0;
     
-    for (i = 1; i < g_SudokuLineSize; ++i){
+    for (i = 1; i <= g_SudokuLineSize; ++i){
       if (mat[i][0] == g_SudokuLineSize - 1){
-        var v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        for (j = 1; j <- g_SudokuLineSize; ++j){
+        let v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (j = 1; j <= g_SudokuLineSize; ++j){
           ++v[mat[i][j]];
           if (mat[i][j] == 0){ a = j; j = g_SudokuLineSize + 1;}
         }
@@ -261,15 +270,15 @@ function solve(){
             ++mat[i][0];
             ++mat[0][a];
             ++z[zoneOf(i, a)];
-            j = g_SudokuLineSize;
+            j = g_SudokuLineSize + 1;
           }
         }
       }
     }
     for (j = 1; j <= g_SudokuLineSize; ++j){
       if (mat[0][j] == g_SudokuLineSize - 1){
-        var v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        for (i = 1; i < g_SudokuLineSize; ++i){
+        let v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (i = 1; i <= g_SudokuLineSize; ++i){
           ++v[mat[i][j]];
           if (mat[i][j] == 0){ a = i; i = g_SudokuLineSize + 1;}
         }
@@ -290,8 +299,8 @@ function solve(){
     for (n = 1; n <= g_SudokuLineSize; ++n){
       if (z[n] == g_SudokuLineSize - 1)
       {
-        var v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        i = Math.trunc(n / 3);
+        var v = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        i = Math.trunc(n / g_SudokuZoneSize);
         if (n % g_SudokuZoneSize != 0){ ++i;}
         j = n - (i - 1) * g_SudokuZoneSize;
         i = (i - 1) * g_SudokuZoneSize + 1;
@@ -299,7 +308,7 @@ function solve(){
         for (k = i; k < i + g_SudokuZoneSize; ++k){
           for (l = j; l < j + g_SudokuZoneSize; ++l){
             ++v[mat[k][l]];
-            if (mat[k][l] == 0){ a = k; b = l; k = l = g_SudokuLineSize + 1;}
+            if (mat[k][l] == 0){ a = k; b = l; k = l = g_SudokuLineSize;}
           }
         }
         for (i = 1; i <= g_SudokuLineSize; ++i){
@@ -326,56 +335,58 @@ function solve(){
       }
   }
   
-  function verif (i, j, k) {
-    for (var a = 1; a < j; ++a) {
-        if (mat[i][a] == k) {return 0;}
+  function verif (i, j, k){
+    let a;
+    for (a = 1; a < j; ++a){
+        if (mat[i][a] == k){ return 0;}
     }
-    for (var a = j + 1; a < g_SudokuLineSize; ++a) {
-        if (mat[i][a] == k) {return 0;}
+    for (a = j + 1; a <= g_SudokuLineSize; ++a){
+        if (mat[i][a] == k){ return 0;}
     }
-    for (a = 1; a < i; ++a) {
-        if (mat[a][j] == k) {return 0;}
+    for (a = 1; a < i; ++a){
+        if (mat[a][j] == k){ return 0;}
     }
-    for (a = i + 1; a < g_SudokuLineSize; ++a) {
-        if (mat[a][j] == k) {return 0;}
+    for (a = i + 1; a <= g_SudokuLineSize; ++a){
+        if (mat[a][j] == k){ return 0;}
     }
     
-    if (i < 4){ i = 1;}
-        else{ if (i > 6){ i = 7;}
-              else{ i = 4;}}
-    if (j < 4){ j = 1;}
-        else {
-            if (j > 6) {j = 7;}
-            else       {j = 4;}
-        }
+    function zoneStart (coord) {return Math.trunc((coord - 1) / g_SudokuZoneSize) * g_SudokuZoneSize + 1;}
+
+    i = zoneStart(i);
+    j = zoneStart(j);    
     
-    var nr = 0;
-    for (var a = i ; a < i + g_SudokuZoneSize ; ++a) {
-        for (var b = j; b < j + g_SudokuLineSize; ++b) {
-            if (mat[a][b] == k) {++nr;}
+    let nr = 0;
+    for (let a = i ; a < i + g_SudokuZoneSize ; ++a){
+        for (let b = j; b < j + g_SudokuZoneSize; ++b){
+            if (mat[a][b] == k){ ++nr;}
         }
     }
     
     return nr == 0;
   }
   
-  function bac (k) {
-    if (k == n + 1) {
-      for (var i = 1; i <= g_SudokuLineSize; ++i) {
-        for (var j = 1; j <= g_SudokuLineSize; ++j) {
-          boxes[g_SudokuLineSize * (i - 1) + j - 1].innerHTML = String(mat[i][j]) + " ";
+  function bac (k){
+    if (k == n + 1){
+      for (i = 1; i <= g_SudokuLineSize; ++i){
+        for (j = 1; j <= g_SudokuLineSize; ++j){
+          boxes[g_SudokuLineSize * (i - 1) + j - 1].innerHTML =
+          ((mat[i][j] == 0) ? "# " : (mat[i][j] - (g_sudokuType != "Hex" ? 0 : 1)).toString(16) + " ");
         }
       }
-      n = -1;
+      n = 0;
     }
-    if (k > n) {return;}
-    for (var ii = 1; ii <= g_SudokuLineSize; ++ii) {
-      if (verif (ln[k], col[k], ii)) {
+    if (k > n){return;}
+
+    for (let ii = 1 ; ii <= g_SudokuLineSize; ++ii){
+      if (verif (ln[k], col[k], ii)){
         mat[ln[k]][col[k]] = ii;
         bac(k + 1);
         mat[ln[k]][col[k]] = 0;
       }
     }
   }
-  bac(1);  
+  bac(1); 
+  let finSolve = new Date();
+  alert (finSolve - startSolve);
+  if (n != 0) {alert("Impossible to Solve!");} 
 }
