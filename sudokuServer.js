c0ceb335d40cd13216e9114dffe803f3b9eb68f3
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2020-01-08 10:36:06 +0200

    Server

--- sudokuServer.js
+++ sudokuServer.js
@@ -1,20 +1,29 @@
-var http=require('http') 
+// node Documents/GitHub/Web_Technics-Project/sudokuServer.js
+var http = require ('http');
+var fs   = require ('fs');
+var path = require('path');
 var server=http.createServer((function (request,response){
-    response.writeHead(200,{"Content-Type":"text\plain"});
-    if(request.method == "GET")
-        {
-            response.end("received GET request.")
+    if (request.method == 'GET') {
+        fs.readFile(path.join(__dirname, 'sudoku.html'), null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('sudoku.html file not found');
+            } else {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+    else {
+        if (request.method == 'POST') {
+            //console.log(request);
+            //console.log (JSON.parse(request.data));
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end(JSON.stringify("SERVER"));
         }
-    else if(request.method == "POST")
-        {
-            response.end("received POST request.");
-        }
-    else
-        {
-            response.end("Undefined request .");
-        }
-    //console.log(JSON.parse(request));
-    response.end();
+        else {console.log ("Undefined method!");}
+    }
 }));
-server.listen(7000); // serverul este pornit si asculta cereri la portul 7000 al masinii locale
-console.log ('Serverul creat asteapta cereri la http://localhost:7000/');
+server.listen(7000);
+console.log ('Waiting requests at http://localhost:7000/');
